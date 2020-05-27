eb67a70d1bd6ad23d267ce30cbe5aa3280fe9929
niubegin <niubegin@gmail.com>
2020-05-27 10:55:32 +0800

    JZ10斐波那契数列

--- src/main/java/sf/jz/JZ10Fibonacci.java
+++ src/main/java/sf/jz/JZ10Fibonacci.java
@@ -1,15 +1,19 @@
 package sf.jz;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class JZ10Fibonacci {
+
     public static void main(String[] args) {
-        System.out.println(fn(39));
+        log.info("{}", fn(10));
+        log.info("{}", fn(3000));
+        //int会溢出
+        log.info("{}", fn(5000));
     }
 
     /**
      * f(0)=0,f(1)=1,f(n)=f(n-1)+f(n-2)
-     *
-     * @param n
-     * @return
      */
     private static int fn(int n) {
         if (n == 0) {
