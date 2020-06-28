44e5705f0cea4334597e9d1dcb2ae2495c5954df
niubegin <niubegin@gmail.com>
2020-06-28 09:22:25 +0800

    JZ38字符串的排列-优化

--- src/main/java/sf/jz/JZ38Permutation.java
+++ src/main/java/sf/jz/JZ38Permutation.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class JZ38Permutation {
@@ -11,6 +12,7 @@ public class JZ38Permutation {
     public static void main(String[] args) {
         char[] chars = {'a', 'b', 'c'};
         permutate(chars, "");
+        permutation("abc");
     }
 
     private static void permutate(char[] chars, String pre) {
@@ -37,6 +39,36 @@ public class JZ38Permutation {
             permutate(newChars, pre);
             //本次字符使用完后移除
             pre = pre.substring(0, pre.length() - 1);
+        }
+    }
+
+    /**
+     * 给输入的str字符串中的字符进行全排列
+     */
+    private static void permutation(String str) {
+        //如果字符串为空，直接返回
+        if (StringUtils.isEmpty(str)) {
+            return;
+        }
+        //否则将字符串转换为字符数字，并从字符0位置开始进行全排列
+        permutation(str.toCharArray(), 0);
+    }
+
+    private static void permutation(char[] chars, int pos) {
+        if (pos == chars.length - 1) {
+            log.info("{}", chars);
+        }
+        for (int i = pos; i < chars.length; i++) {
+            //首部字符和它后面的字符（包括自己）进行交换
+            char temp = chars[i];
+            chars[i] = chars[pos];
+            chars[pos] = temp;
+            //递归求后面的字符的排列
+            permutation(chars, pos + 1);
+            //由于前面交换了一下，所以chs的内容改变了，我们要还原回来
+            temp = chars[i];
+            chars[i] = chars[pos];
+            chars[pos] = temp;
         }
     }
 }
