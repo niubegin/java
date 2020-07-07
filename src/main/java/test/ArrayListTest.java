93f9f163ba60b012ae40e9e6486a0a08a69ad3b2
niubegin <niubegin@gmail.com>
2020-07-07 21:05:18 +0800

    列表去空

--- src/main/java/test/ArrayListTest.java
+++ src/main/java/test/ArrayListTest.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+@Slf4j
 public class ArrayListTest {
 
     public static void main(String[] args) {
@@ -15,7 +18,20 @@ public class ArrayListTest {
         List<String> res = toAddList.stream().filter(item -> item.equals("2")).collect(Collectors.toList());
         list.addAll(res);
         List<String> nullList = null;
-        nullList.stream().filter(item -> item.equals("2")).collect(Collectors.toList());
-        list.addAll(nullList);
+//        nullList.stream().filter(item -> item.equals("2")).collect(Collectors.toList());
+//        list.addAll(nullList);
+
+        List<String> hasEmptyStrList = new ArrayList<>();
+        hasEmptyStrList.add("a");
+        hasEmptyStrList.add("");
+        hasEmptyStrList.add("c");
+        hasEmptyStrList.add(" ");
+        hasEmptyStrList.add("d");
+        hasEmptyStrList.add(" ");
+        log.info("{}", hasEmptyStrList);
+        hasEmptyStrList.stream().filter(StringUtils::isNotBlank).collect(Collectors.toList());
+        log.info("{}", hasEmptyStrList);
+        hasEmptyStrList = hasEmptyStrList.stream().filter(StringUtils::isNotBlank).collect(Collectors.toList());
+        log.info("{}", hasEmptyStrList);
     }
 }
