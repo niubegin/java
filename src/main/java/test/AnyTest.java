92fbf70437c9c41e8097ba542686933aa71f97e4
niubegin <niubegin@gmail.com>
2020-07-24 09:53:20 +0800

    测试1

--- src/main/java/test/AnyTest.java
+++ src/main/java/test/AnyTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.math.BigDecimal;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -7,5 +8,23 @@ public class AnyTest {
 
     public static void main(String[] args) {
         log.info("{}", 2 / 2);
+        log.info("{}", "" + null);
+        Double sumVolume = 2997d;
+        Long shelfVolume = 2500000000L;
+        compute(sumVolume, shelfVolume);
+        compute(100800d, shelfVolume);
+    }
+
+    private static void compute(Double sumVolume, Long shelfVolume) {
+        Double vacancy = 1 - sumVolume / shelfVolume;
+        log.info("{}", vacancy);
+        if (vacancy > 1) {
+            vacancy = 1.00;
+        } else if (vacancy < 0) {
+            vacancy = 0.00;
+        }
+        BigDecimal bigDecimal = new BigDecimal(vacancy);
+        double vacancyReal = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        log.info("{}", vacancyReal);
     }
 }
