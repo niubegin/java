ab92d1b97e197d1e5398317578072ba13c3eb0dc
niubegin <niubegin@gmail.com>
2020-07-29 10:56:22 +0800

    字符串测试

--- src/main/java/test/StringTest.java
+++ src/main/java/test/StringTest.java
@@ -7,6 +7,63 @@ import org.apache.commons.lang3.StringUtils;
 public class StringTest {
 
     public static void main(String[] args) {
+        //取值>=1
+        paddingZeroAtLeft(0);
+        paddingZeroAtLeft(1);
+        paddingZeroAtLeft(9);
+        paddingZeroAtLeft(98);
+        paddingZeroAtLeft(99);
+        paddingZeroAtLeft(100);
+        paddingZeroAtLeft(101);
+        Integer len = null;
+        log.info("{}", StringUtils.join("PRE", StringUtils.repeat('_', len)));
+        log.info("{}", StringUtils.join("PRE", StringUtils.repeat('_', 4)));
+        log.info("{}", getSerialNumFromContainerCode("aaaa999999999aaaa", "aaaa_________aaaa", 9));
+        log.info("{}", getSerialNumFromContainerCode("aaaa9999999990", "aaaa__________aaaa", 10));
+        log.info("{}", getSerialNumFromContainerCode("aaaa1234", "aaaa____", 4));
+        log.info("{}", getSerialNumFromContainerCode("1234", "____", 4));
+        log.info("{}", getSerialNumFromContainerCode("abcd", "____", 4));
+        log.info("{}", Integer.MAX_VALUE);
+        log.info("{}", Integer.valueOf("00001") + 1);
+//        orderStrings();
+    }
+
+    private static int getSerialNumFromContainerCode(String containerCode, String containerCodeLike,
+        int serialNumLength) {
+        try {
+            char[] containerCodeChars = containerCode.toCharArray();
+            char[] containerCodeLikeChars = containerCodeLike.toCharArray();
+            char[] serialNumChars = new char[serialNumLength];
+            int serialNumCharsIndex = 0;
+            for (int index = 0; index < containerCodeChars.length; index++) {
+                if (containerCodeChars[index] != containerCodeLikeChars[index]) {
+                    serialNumChars[serialNumCharsIndex] = containerCodeChars[index];
+                    serialNumCharsIndex++;
+                    if (serialNumCharsIndex == serialNumLength) {
+                        break;
+                    }
+                }
+            }
+
+            log.info("容器号：{}，容器号匹配串：{}，解析到序列号：{}", containerCode, containerCodeLike, serialNumChars);
+            return Integer.parseInt(String.valueOf(serialNumChars));
+        } catch (Exception e) {
+            log.warn("容器号：{}，容器号前缀：{}，获取序列号异常：", containerCode, containerCodeLike, e);
+            return 0;
+        }
+    }
+
+    private static void paddingZeroAtLeft(int num) {
+        int t = 999999999;
+        int serialNumLength = 9;
+        int max = (int) Math.pow(10, serialNumLength) - 1;
+        //取值范围0-98
+        int mod = (num % max) + 1;
+        String result = String.format("%0" + serialNumLength + "d", mod);
+        log.info("max:{},result:{},num:{}", max, result, num);
+    }
+
+    private static void orderStrings() {
         try {
             String autoBindingContainerGeneratorOrderRule = StringUtils.defaultString("43321", "123");
             String[] itemArray = {"dynamicContent", "fixContent", "serialNum"};
