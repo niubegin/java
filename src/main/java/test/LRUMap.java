fcd5b64595b3bde8e465f5038654e22d4ff4cfdb
niubegin <niubegin@gmail.com>
2020-07-30 16:40:06 +0800

    LRU缓存

--- src/main/java/test/LRUMap.java
+++ src/main/java/test/LRUMap.java
@@ -20,8 +20,21 @@ public class LRUMap {
                 return size() > cacheSize;
             }
         };
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
 
     public static void main(String[] args) {
+        int cap = (int) Math.ceil(500 / 0.75f) + 1;
+        log.info("{},{}", cap, tableSizeFor(cap));
         lruCache.put("AAA___BBBB1", 1);
         lruCache.put("AAA___BBBB2", 1);
         lruCache.put("AAA___BBBB3", 1);
