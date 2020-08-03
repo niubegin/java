628a30ae3726454aba41ab1442346c9ddbe49811
niubegin <niubegin@gmail.com>
2020-08-03 18:56:41 +0800

    JZ49丑数

---
+++ src/main/java/sf/jz/JZ49GetUglyNumber.java
@@ -0,0 +1,18 @@
+package sf.jz;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JZ49GetUglyNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 查找第index个丑数；因子只有2，3，5的数；1是第一个；1，2，3，4，5，6，8，9，10，12，15……
+     */
+    private static int get(int index) {
+        return 0;
+    }
+}
