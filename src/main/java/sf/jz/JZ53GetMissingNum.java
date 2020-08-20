99482591d05d49c0152045632e6cea541815c364
niubegin <niubegin@gmail.com>
2020-08-20 10:56:57 +0800

    JZ53获取0-n-1中缺少的数字

--- src/main/java/sf/jz/JZ53GetMissingNum.java
+++ src/main/java/sf/jz/JZ53GetMissingNum.java
@@ -5,9 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JZ53GetMissingNum {
 
+    /**
+     * 要点：二分；边界情况；
+     */
     public static void main(String[] args) {
         int[] arr = {0, 1, 2, 4, 5, 6, 7};
         log.info("{}", get(arr, 0, arr.length - 1));
+        int[] arr1 = {0};
+        log.info("{}", get(arr1, 0, arr1.length - 1));
+        int[] arr2 = {1};
+        log.info("{}", get(arr2, 0, arr2.length - 1));
     }
 
     private static int get(int[] arr, int begin, int end) {
@@ -21,7 +28,8 @@ public class JZ53GetMissingNum {
             return get(arr, mid + 1, end);
         } else {
             if (mid < 1) {
-                return -1;
+                // 没有前一个时，返回本身；注意边界情况
+                return mid;
             } else {
                 if (arr[mid - 1] == mid - 1) {
                     // 检查前一个，相等的话就是本身
