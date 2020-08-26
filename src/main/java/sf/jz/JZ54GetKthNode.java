31da9867f4828966913460c21d62a319e8f59fff
niubegin <niubegin@gmail.com>
2020-08-26 11:06:08 +0800

    JZ54二叉搜索树的第k大节点

---
+++ src/main/java/sf/jz/JZ54GetKthNode.java
@@ -0,0 +1,51 @@
+package sf.jz;
+
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JZ54GetKthNode {
+
+    public static void main(String[] args) {
+        Integer[] nodes = {5, 3, 7, 2, 4, 6, 8};
+        TreeNode tmp = TreeNode.build(nodes);
+        log.info("{}", tmp);
+        log.info("{}", get(tmp, 3));
+    }
+
+    private static int count = 0;
+
+    private static TreeNode get(TreeNode node, int kth) {
+        TreeNode target = null;
+        if (Objects.nonNull(node.getLeft())) {
+            target = get(node.getLeft(), kth);
+            if (Objects.nonNull(target)) {
+                return target;
+            }
+        }
+
+        // count 计数多了一个变量，可以用kth--替代实现
+        /*
+        count++;
+        if (count == kth) {
+            return node.getIntValue();
+        }
+         */
+        kth--;
+        if (kth == 1) {
+            return node;
+        }
+
+        // log.debug("{}", node.getIntValue());
+
+        if (Objects.nonNull(node.getRight())) {
+            target = get(node.getRight(), kth);
+            if (Objects.nonNull(target)) {
+                return target;
+            }
+        }
+
+        return null;
+    }
+
+}
